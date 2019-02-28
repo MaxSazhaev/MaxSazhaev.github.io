1e0fbf7dbd4a16c7242d2a43ed310cd6ecd58679
Max Sazhaev <maxsazhaev@gmail.com>
2019-02-28 16:15:52 -0500

    added propper images and gifs to all gaming projects

--- js/main.js
+++ js/main.js
@@ -24,3 +24,50 @@ jQuery(document).ready(function () {
         }
     })
 });
+
+$(document).ready(function()
+{
+    $("#396Final").hover(
+        function()
+        {
+            $(this).attr("src", "gifs/396FinalGIF.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/396FinalIMG.jpg");
+        }                         
+    );     
+    
+    $("#231FunGame").hover(
+        function()
+        {
+            $(this).attr("src", "gifs/231FunGameGIF.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/231FunGameIMG.jpg");
+        }                         
+    );    
+
+    $("#396Assignment2").hover(
+        function()
+        {
+            $(this).attr("src", "gifs/396Assignment2GIF.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/396Assignment2IMG.jpg");
+        }                         
+    );    
+
+    $("#Impulse").hover(
+        function()
+        {
+            $(this).attr("src", "gifs/ImpulseGIF.gif");
+        },
+        function()
+        {
+            $(this).attr("src", "images/ImpulseIMG.jpg");
+        }                         
+    );   
+});
